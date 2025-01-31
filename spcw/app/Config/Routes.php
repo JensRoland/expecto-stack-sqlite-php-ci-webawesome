416c8f1124b9882b50dae791200979ec00c0eb62
Jens Roland <mail@jensroland.com>
2025-01-31 03:21:51 +0100

    feat: add CodeIgniter Shield authentication

--- spcw/app/Config/Routes.php
+++ spcw/app/Config/Routes.php
@@ -11,3 +11,4 @@ use CodeIgniter\Router\RouteCollection;
 // http://example.com/hello-world/hello/1
 // will call the controller App\Controllers\HelloWorld
 // and invoke its method getHello() with the parameter '1'.
+service('auth')->routes($routes);
